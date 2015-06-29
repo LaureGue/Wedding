b9149d1e83d6591d18074c1f49974aac2e058830
LaureGue <calamity_jane263@hotmail.com>
2015-06-29 17:44:49 +0200

    Update fireflies.js

--- js/fireflies.js
+++ js/fireflies.js
@@ -1,7 +1,7 @@
 
     $(document).ready(function() {
         $.firefly({
-            images : ['http://www.itsfirefly.com/images/fly1by1.png','http://www.itsfirefly.com/images/fly2by2.png'],   //Fly images        
+            images : ['Jb-Po-Wedding/images/1434936070_common_heart_like_favourite_favorite.png','http://www.itsfirefly.com/images/fly2by2.png'],   //Fly images        
             total : 100, //number of flies
             on: '#invitation' //id of div
         });
