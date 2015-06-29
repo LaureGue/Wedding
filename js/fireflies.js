81ea202da96550aac3ca77ae3fba4c6f0adf78d2
Laure <calamity_jane263@hotmail.com>
2015-06-29 19:51:21 +0200

    fireflies

--- js/fireflies.js
+++ js/fireflies.js
@@ -1,7 +1,7 @@
 
     $(document).ready(function() {
         $.firefly({
-            images : ['Jb-Po-Wedding/images/1434936070_common_heart_like_favourite_favorite.png','http://www.itsfirefly.com/images/fly2by2.png'],   //Fly images        
+            images : ['Jb-Po-Wedding/images/1434936070_common_heart_like_favourite_favorite.png','Jb-Po-Wedding/images/pixel_5.png'],   //Fly images        
             total : 100, //number of flies
             on: '#invitation' //id of div
         });
