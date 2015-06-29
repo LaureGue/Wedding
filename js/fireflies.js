748ff2e2e4c8e5731dcf2393bbf5a47f5b4287a2
LaureGue <calamity_jane263@hotmail.com>
2015-06-29 16:35:13 +0200

    Create fireflies.js

---
+++ js/fireflies.js
@@ -0,0 +1,9 @@
+<script type="text/javascript">
+    $(document).ready(function() {
+        $.firefly({
+            images : ['http://www.itsfirefly.com/images/fly1by1.png','http://www.itsfirefly.com/images/fly2by2.png'],   //Fly images        
+            total : 100, //number of flies
+            on: '#invitation' //id of div
+        });
+    });
+</script>
