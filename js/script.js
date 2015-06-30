d7b65110808bb8c34e55730beb1e78cc6b02cf40
LaureGue <calamity_jane263@hotmail.com>
2015-06-30 17:53:03 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,25 +1,4 @@
-function afficheMenu(obj){
+function myFunction() {
    
-   var idmain_menu_btn    = obj.id;
-   var idmain_menu_block = 'sous' + idMenu;
-   var main_menu_block  = document.getElementById(main_menu_block);
-   
-   /*****************************************************/
-   /**   on cache tous les sous-menus pour n'afficher    **/
-   /** que celui dont le menu correspondant est cliqué **/
-   /** où 4 correspond au nombre de sous-menus        **/
-   /*****************************************************/
- 
-   
-   if(main_menu_block){
-      //alertmain_menu_block.style.display);
-      if(main_menu_block.style.display == "block"){
-         main_menu_block.style.display = "none";
-      }
-      else{
-         main_menu_block.style.display = "block";
-      }
-   }
+   document.getElementById("main_menu_block").style.display = "none";
 }
-
-
