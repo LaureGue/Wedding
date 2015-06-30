2bd64802dfae93c40e2f2f456b1a198afd080e4b
LaureGue <calamity_jane263@hotmail.com>
2015-06-30 17:51:19 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,25 @@
-$(document).ready(function() {
-       $(".main_menu_btn").click(function() {
-    $(".main_menu_btn").animate({ marginLeft: "170px"  }, 400 );
-  },function(){
-    $(".main_menu_btn").animate({ marginLeft: "0"  }, 300 );  });
-     $(".main_menu_btn").hover(function() {
-      },function(){
-    $(".main_menu_btn").animate({ marginLeft: "0"  }, 300 );
-  });
-});
-
-
+function afficheMenu(obj){
+   
+   var idmain_menu_btn    = obj.id;
+   var idmain_menu_block = 'sous' + idMenu;
+   var main_menu_block  = document.getElementById(main_menu_block);
+   
+   /*****************************************************/
+   /**   on cache tous les sous-menus pour n'afficher    **/
+   /** que celui dont le menu correspondant est cliqué **/
+   /** où 4 correspond au nombre de sous-menus        **/
+   /*****************************************************/
+ 
+   
+   if(main_menu_block){
+      //alertmain_menu_block.style.display);
+      if(main_menu_block.style.display == "block"){
+         main_menu_block.style.display = "none";
+      }
+      else{
+         main_menu_block.style.display = "block";
+      }
+   }
+}
 
 
