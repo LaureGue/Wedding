9afe153a8e185e535e834776b3acc0e81b18987f
LaureGue <calamity_jane263@hotmail.com>
2015-06-30 18:01:13 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
-function myFunction() {
-   
-   document.getElementById("main_menu_block").style.display = "none";
-}
+window.onload=function(){
+    var bouton = document.getElementById('btnMenu');
+    var nav = document.getElementById('nav');
+    bouton.onclick = function(e){
+        if(nav.style.display=="block"){
+            nav.style.display="none";
+        }else{
+            nav.style.display="block";
+        }
+    };
+};
